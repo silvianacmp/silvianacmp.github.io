5c91555fcc170695b68a30acc848880992e1cd00
silvianac <silvianac@telenav.com>
2017-03-15 00:16:41 +0200

    Css changes and additional info added to app.

--- src/App.js
+++ src/App.js
@@ -9,24 +9,34 @@ class App extends Component {
         return (
             <div className="App">
                 <div className="header">
-                    <h2>Silviana Cimpian</h2>
-                    <h3>Computer Science student</h3>
+                    <div className="header-content">
+                        <h1>Hi, I'm Silviana</h1>
+                    </div>
                 </div>
-                <div className="links">
-                    <list>
-                        <li>
-                            <i className="fa fa-linkedin" aria-hidden="true"></i>
-                            <a href="https://www.linkedin.com/in/silviana-ioana-cimpian/" target="_blank">LinkedIn</a>
-                        </li>
-                        <li>
-                            <i className="fa fa-paper-plane" aria-hidden="true"></i>
-                            <a href="mailto:silvianacimpian@gmail.com">Contact</a>
-                        </li>
-                        <li>
-                            <i className="fa fa-github" aria-hidden="true"></i>
-                            <a href="https://github.com/silvianacmp" target="_blank">Github</a>
-                        </li>
-                    </list>
+                <div className="body">
+                    <div className="about">
+                        <h2>Computer Science student</h2>
+                        <h3>Currently living in Cluj-Napoca</h3>
+                        <h4>Data Science enthusiast</h4>
+                        <h4>Deep Learning learner</h4>
+                        <h4>Front end fiddler</h4>
+                    </div>
+                    <div className="links">
+                        <list>
+                            <li>
+                                <i className="fa fa-linkedin" aria-hidden="true"></i>
+                                <a href="https://www.linkedin.com/in/silviana-ioana-cimpian/" target="_blank">LinkedIn</a>
+                            </li>
+                            <li>
+                                <i className="fa fa-paper-plane" aria-hidden="true"></i>
+                                <a href="mailto:silvianacimpian@gmail.com">Contact</a>
+                            </li>
+                            <li>
+                                <i className="fa fa-github" aria-hidden="true"></i>
+                                <a href="https://github.com/silvianacmp" target="_blank">Github</a>
+                            </li>
+                        </list>
+                    </div>
                 </div>
             </div>
         );
