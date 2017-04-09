35b5c67a5a1df0c0bbfcae284cbad469019307db
silvianac <silvianac@telenav.com>
2017-04-09 23:12:27 +0300

    Align the links.

--- src/App.js
+++ src/App.js
@@ -24,15 +24,15 @@ class App extends Component {
                     <div className="links">
                         <list>
                             <li>
-                                <i className="fa fa-linkedin" aria-hidden="true"></i>
+                                <i className="fa fa-linkedin small" aria-hidden="true"></i>
                                 <a href="https://www.linkedin.com/in/silviana-ioana-cimpian/" target="_blank">LinkedIn</a>
                             </li>
                             <li>
-                                <i className="fa fa-paper-plane" aria-hidden="true"></i>
+                                <i className="fa fa-paper-plane large" aria-hidden="true"></i>
                                 <a href="mailto:silvianacimpian@gmail.com">Contact</a>
                             </li>
                             <li>
-                                <i className="fa fa-github" aria-hidden="true"></i>
+                                <i className="fa fa-github small" aria-hidden="true"></i>
                                 <a href="https://github.com/silvianacmp" target="_blank">Github</a>
                             </li>
                         </list>
